73df08b12dbd68fa4c3b5fe68a249d01e5f1d8ea
Francois Lefebvre <23463958+fslef@users.noreply.github.com>
2025-10-01 15:37:31 +0200

    feat: remove unused localization config settings

--- source/Private/Config/Set-DSDefaultConfig.ps1
+++ source/Private/Config/Set-DSDefaultConfig.ps1
@@ -6,13 +6,12 @@ function Set-DSDefaultConfig {
 
         Write-PSFMessage -Module DataSanitizer -Level Important -String 'Import-DSConfig.defaults'
 
-        Set-PSFConfig -Module DataSanitizer -Name Localization.Language -Value "fr-FR" -Validation string -Initialize -Description "The language the current DataSanitizer session will use for console display" -ModuleExport
-        Set-PSFConfig -Module DataSanitizer -Name Localization.LoggingLanguage -Value "fr-FR" -Validation string -Initialize -Description "The language the current DataSanitizer session will use for logging" -ModuleExport
         Set-PSFConfig -Module DataSanitizer -Name Logging.LogFile.CsvDelimiter -Value ';' -Validation string -Initialize -Description "The text delimiter used when exporting to CSV." -ModuleExport
         Set-PSFConfig -Module DataSanitizer -Name Text.Encoding.Default -Value 'utf8' -Validation string -Initialize -Description "The default text encoding to use when reading and writing text files." -ModuleExport
         Set-PSFConfig -Module DataSanitizer -Name Path.DSrootFolder -Value '<tbd>' -Validation string -Initialize -Description "The root folder for DataSanitizer." -ModuleExport
-        Set-PSFConfig -Module DataSanitizer -Name Path.DSConfigFile -Value '<tbd>' -Validation string -Initialize -Description "The full path to the DataSanitizer configuration file." -ModuleExport
-        Set-PSFConfig -Module DataSanitizer -Name Path.DSIncidentFolder -Value '<tbd>' -Validation string -Initialize -Description "The root incident folder where incident files are stored." -ModuleExport
+        Set-PSFConfig -Module DataSanitizer -Name Path.DSConfigFile -Value '<tbd>' -Validation string -Initialize -Description "The config file for DataSanitizer." -ModuleExport
+        Set-PSFConfig -Module DataSanitizer -Name Path.DSLogFolder -Value '<tbd>' -Validation string -Initialize -Description "The root incident folder where incident files are stored." -ModuleExport
 
     }
 }
+    
