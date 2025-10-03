5fc87c5ca79933007bd5a29216f85e00a8344ccb
Francois Lefebvre <23463958+fslef@users.noreply.github.com>
2025-10-03 15:19:51 +0200

    refactor: enhance initialization process by adding configuration settings and improving message logging

--- source/Private/Initialize-DSModuleSession.ps1
+++ source/Private/Initialize-DSModuleSession.ps1
@@ -4,10 +4,17 @@ function Initialize-DSModuleSession {
 
     process {
 
-        Clear-PSFMessage
-        
+
         Import-PSFLocalizedString -Module DataSanitizer -Path (Join-Path $PSScriptRoot -ChildPath './Localization/en-US.psd1') -Language 'en-US'
         Import-PSFLocalizedString -Module DataSanitizer -Path (Join-Path $PSScriptRoot -ChildPath './Localization/fr-FR.psd1') -Language 'fr-FR'
+
+        Write-PSFMessage -Module DataSanitizer -Level Important -String 'Import-DSConfig.defaults'
+
+        Set-PSFConfig -Module DataSanitizer -Name Logging.LogFile.CsvDelimiter -Value ';' -Validation string -Initialize -Description "The text delimiter used when exporting to CSV." -ModuleExport
+        Set-PSFConfig -Module DataSanitizer -Name Text.Encoding.Default -Value 'utf8' -Validation string -Initialize -Description "The default text encoding to use when reading and writing text files." -ModuleExport
+        Set-PSFConfig -Module DataSanitizer -Name Path.DSrootFolder -Value '<tbd>' -Validation string -Initialize -Description "The root folder for DataSanitizer." -ModuleExport
+        Set-PSFConfig -Module DataSanitizer -Name Path.DSConfigFile -Value '<tbd>' -Validation string -Initialize -Description "The config file for DataSanitizer." -ModuleExport
+        Set-PSFConfig -Module DataSanitizer -Name Path.DSLogInputFolder -Value '<tbd>' -Validation string -Initialize -Description "The Log Input folder where log files are stored." -ModuleExport
 
         Set-DSDefaultConfig
 
