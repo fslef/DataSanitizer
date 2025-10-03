7e06aabce070315df9f398919a1150fcda40bf6c
Francois Lefebvre <23463958+fslef@users.noreply.github.com>
2025-10-03 17:54:45 +0200

    refactor: update parameter types to PsfDirectorySingle for improved folder handling in New-DsFolder function

--- source/Public/Config/New-DsFolder.ps1
+++ source/Public/Config/New-DsFolder.ps1
@@ -2,7 +2,7 @@ function New-DsFolder {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
-        [PsfDirectory]$RootFolder,
+        [PsfDirectorySingle]$RootFolder,
 
         [String]$IntermediateFolderName
     )
@@ -25,13 +25,13 @@ function New-DsFolder {
         # Adding _Config Folder under root
         Write-PSFMessage -Module DataSanitizer -Level Verbose -String 'New-DsFolder.AddingConfigFolder' -StringValues $RootFolder
         New-Item -Path $RootFolder -Name "_Config" -ItemType Directory | Out-Null
-        [PsfDirectory] $ConfigFolderPath = Join-Path -Path $RootFolder -ChildPath "_Config"
+        [PsfDirectorySingle] $ConfigFolderPath = Join-Path -Path $RootFolder -ChildPath "_Config"
 
         # Add _Intermediate Folder
         if ($IntermediateFolderName) {
             Write-PSFMessage -Module DataSanitizer -Level Verbose -String 'New-DsFolder.AddingIntermediateFolder' -StringValues $IntermediateFolderName, $RootFolder
             New-Item -Path $RootFolder -Name $IntermediateFolderName -ItemType Directory | Out-Null
-            [PsfDirectory] $IntermediateFolderPath = Join-Path -Path $RootFolder -ChildPath $IntermediateFolderName
+            [PsfDirectorySingle] $IntermediateFolderPath = Join-Path -Path $RootFolder -ChildPath $IntermediateFolderName
 
         }
 
@@ -39,29 +39,33 @@ function New-DsFolder {
         if ($IntermediateFolderName) {
             Write-PSFMessage -Module DataSanitizer -Level Verbose -String 'New-DsFolder.AddingLogFolder' -StringValues $IntermediateFolderPath
             New-Item -Path $IntermediateFolderPath -Name "LogFolder" -ItemType Directory | Out-Null
-            [PsfDirectory] $LogFolderPath = Join-Path -Path $IntermediateFolderPath -ChildPath "LogFolder"
+            [PsfDirectorySingle] $LogFolderPath = Join-Path -Path $IntermediateFolderPath -ChildPath "LogFolder"
 
         }
         else {
             Write-PSFMessage -Module DataSanitizer -Level Verbose -String 'New-DsFolder.AddingLogFolder' -StringValues $RootFolder
             New-Item -Path $RootFolder -Name "LogFolder" -ItemType Directory | Out-Null
-            [PsfDirectory] $LogFolderPath = Join-Path -Path $RootFolder -ChildPath "LogFolder"
+            [PsfDirectorySingle] $LogFolderPath = Join-Path -Path $RootFolder -ChildPath "LogFolder"
 
         }
         # Adding Input Folder under LogFolder
         Write-PSFMessage -Module DataSanitizer -Level Verbose -String 'New-DsFolder.AddingLogInputFolder' -StringValues $LogFolderPath
         New-Item -Path $LogFolderPath -Name "Input" -ItemType Directory | Out-Null
-        [PsfDirectory] $LogInputFolderPath = Join-Path -Path $LogFolderPath -ChildPath "Input"
+        [PsfDirectorySingle] $LogInputFolderPath = Join-Path -Path $LogFolderPath -ChildPath "Input"
 
-        Set-PSFConfig -Module DataSanitizer -Name 'Path.DSLogInputFolder' -Value $LogInputFolderPath
+
+        # Adding working Folder under LogFolder
+        Write-PSFMessage -Module DataSanitizer -Level Verbose -String 'New-DsFolder.AddingLogWorkingFolder' -StringValues $LogFolderPath
+        New-Item -Path $LogFolderPath -Name "WorkingFolder" -ItemType Directory | Out-Null
+        [PsfDirectorySingle] $LogWorkingFolderPath = Join-Path -Path $LogFolderPath -ChildPath "WorkingFolder"
 
         # Adding Json config File
 
         $config = [PSCustomObject]@{
             Version = 1
             Static  = [PSCustomObject]@{
-                "DataSanitizer.path.DSrootFolder"     = $RootFolder
-                "DataSanitizer.path.DSLogInputFolder" = $LogInputFolderPath
+                "DataSanitizer.path.DSrootFolder"     = $RootFolder.path
+                "DataSanitizer.path.DSLogInputFolder" = $LogInputFolderPath.path
             }
         }
 
